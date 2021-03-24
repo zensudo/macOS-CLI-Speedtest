e373ffd5c7e4a7f09c20df8dbed4bcda5894459a
Zen Sudo <32953933+zensudo@users.noreply.github.com>
2021-03-24 12:53:03 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,17 @@
 # macOS CLI Speedtest installer v1
 # https://github.com/zensudo/macOS_CLI_Speedtest
 
+# create folders
+mkdir /usr/local/Cellar/speedtest/;
+mkdir /usr/local/Cellar/speedtest/1.0.0/;
+mkdir /usr/local/Cellar/speedtest/1.0.0/bin/;
+mkdir /usr/local/Cellar/speedtest/1.0.0/share/;
+mkdir /usr/local/Cellar/speedtest/1.0.0/share/man/;
+mkdir /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
+
 # move files to destination folder
 cp ./speedtest /usr/local/Cellar/speedtest/1.0.0/bin/;
-cp ./speedtest.5 ./speedtest /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
+cp ./speedtest.5 /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
 
 # create symlink alias folder
 ln -s /usr/local/Cellar/speedtest /usr/local/opt/speedtest;
@@ -13,6 +21,9 @@ ln -s /usr/local/Cellar/speedtest/1.0.0/bin/speedtest /usr/local/bin/speedtest;
 rm ./README.md;
 rm ./speedtest;
 rm ./speedtest.5;
-rm ../macOS_CLI_Speedtest;
+rm ./installer.sh
+cd ..;
+rmdir macOS_CLI_Speedtest-main
+
 
 exit 0;
