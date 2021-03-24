285b2267e71a0424d37e3ebfc5afa71e55fc45a2
Julian <julian.gerber@klambt.de>
2021-03-24 13:00:28 +0100

    created subfolder

--- installer.sh
+++ installer.sh
@@ -10,8 +10,8 @@ mkdir /usr/local/Cellar/speedtest/1.0.0/share/man/;
 mkdir /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
 
 # move files to destination folder
-cp ./speedtest /usr/local/Cellar/speedtest/1.0.0/bin/;
-cp ./speedtest.5 /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
+cp ./content/speedtest /usr/local/Cellar/speedtest/1.0.0/bin/;
+cp ./content/speedtest.5 /usr/local/Cellar/speedtest/1.0.0/share/man/man5/;
 
 # create symlink alias folder
 ln -s /usr/local/Cellar/speedtest /usr/local/opt/speedtest;
@@ -19,11 +19,10 @@ ln -s /usr/local/Cellar/speedtest/1.0.0/bin/speedtest /usr/local/bin/speedtest;
 
 # remove this package
 rm ./README.md;
-rm ./speedtest;
-rm ./speedtest.5;
-rm ./installer.sh
+rm ./installer.sh;
+rm -r ./content/;
 cd ..;
-rmdir macOS_CLI_Speedtest-main
+rmdir macOS_CLI_Speedtest-main;
 
 
 exit 0;
